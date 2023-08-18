ea6fd4e7d58aaf8f9782d685c35a1a9809ed8e93
Anna-1987 <mag260910@gmail.com>
2023-08-18 21:09:39 +0300

    cards

---
+++ src/cards/services/service-api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes?limit=9&rating=3';
+
+// ?category=Beef&page=1&limit=6&time=160&area=Irish&ingredients=640c2dd963a319ea671e3796
+
+export async function fetchImages(q, page, perPage) {
+    const url = `${BASE_URL}`;
+    const response = await axios.get(url);
+    return response.data;          
+};
