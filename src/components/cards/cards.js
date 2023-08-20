f90820c4610033a3c9288aec9c9266f220866664
andriy <evicandrij@gmail.com>
2023-08-20 19:16:45 +0300

    commit

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,44 +1,42 @@
-import {fetchImages} from './services/service-api';
-import './css/styles.scss';
+// import {fetchImages} from './services/service-api';
+// import './css/styles.scss';
 
 
-const refs ={
-    searchForm:document.querySelector('.search-form'),
-    submitBtn:document.querySelector('.submit'),
-    gallery:document.querySelector('.gallery'),
-    loadMoreBtn:document.querySelector('.load-more'),
-}
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes?limit=9&rating=3';
-fetch(`${BASE_URL}`).then((response)=> response.json()).then(data => {
-        console.log(data);
+// const refs ={
+//     searchForm:document.querySelector('.search-form'),
+//     submitBtn:document.querySelector('.submit'),
+//     gallery:document.querySelector('.gallery'),
+//     loadMoreBtn:document.querySelector('.load-more'),
+// }
+// const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes?limit=9&rating=3';
+// fetch(`${BASE_URL}`).then((response)=> response.json()).then(data => {
 
-        const searchResults = data.results;
-        console.log(searchResults);
+//         const searchResults = data.results;
 
-        createGalleryCard(searchResults);
-        refs.gallery.insertAdjacentHTML("beforeend", createGalleryCard(searchResults));
-      })
+//         createGalleryCard(searchResults);
+//         refs.gallery.insertAdjacentHTML("beforeend", createGalleryCard(searchResults));
+//       })
 
-      function createGalleryCard(searchResults){
-        return searchResults.map(({ preview, title, description}) => {
-            const desktop = description.slice(0,62)
-             const mobile  = description.slice(0,97)
-            let text = mobile + '...';
-            if(window.screen.width >= 768){
-                text = desktop + '...'
-            }
-            return `<div class="photo-card">
-            <div class = "backdrop"></div>
-                <img src="${preview}" alt="${title}"/>
-                <div class="info">
-                <div class="info-text">
-                <h3 class="info-item">${title}</h3>
-                <p class="info-text">${text}</p>
-                </div>
-            </div>
-            <button type="button" class="btn-see-recipe">See recipe</button>
-            </div>`
-        }).join('');
-    }
+//       function createGalleryCard(searchResults){
+//         return searchResults.map(({ preview, title, description}) => {
+//             const desktop = description.slice(0,62)
+//              const mobile  = description.slice(0,97)
+//             let text = mobile + '...';
+//             if(window.screen.width >= 768){
+//                 text = desktop + '...'
+//             }
+//             return `<div class="photo-card">
+//             <div class = "backdrop"></div>
+//                 <img class="img-card" src="${preview}" alt="${title}"/>
+//                 <div class="info">
+//                 <div class="info-text">
+//                 <h3 class="info-item">${title}</h3>
+//                 <p class="info-text">${text}</p>
+//                 </div>
+//             </div>
+//             <button type="button" class="btn-see-recipe">See recipe</button>
+//             </div>`
+//         }).join('');
+//     }
 
     
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import cart from './components/cards/cards.js'
+import Categoriesss from './components/Categories/Categories.js'
+
