cf43806aa21e05fb8c2eb2f5175ccbc51186396a
andriy <evicandrij@gmail.com>
2023-08-20 20:27:14 +0300

    comit

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,6 +1,5 @@
 import { axiosRecipes } from './axiosFilters';
 import { axiosCard } from './axiosCard';
-import {createGalleryCard} from '../CartRecipe/CartRecipe.js'
 
 
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
@@ -270,5 +269,32 @@ activeCategories  = e.target;
 e.target.classList.add('active');
 
 })
+
+function createGalleryCard(searchResults){
+  if(searchResults === []){
+   console.log('fdsvds')
+  }
+  else{
+    return searchResults.map(({ preview, title, description}) => {
+      const desktop = description.slice(0,62)
+       const mobile  = description.slice(0,97)
+      let text = mobile + '...';
+      if(window.screen.width >= 768){
+          text = desktop + '...'
+      }
+      return `<div class="photo-card">
+      <div class = "backdrop"></div>
+          <img class="img-card" src="${preview}" alt="${title}"/>
+          <div class="info">
+          <div class="info-text">
+          <h3 class="info-item">${title}</h3>
+          <p class="info-text">${text}</p>
+          </div>
+      </div>
+      <button type="button" class="btn-see-recipe">See recipe</button>
+      </div>`
+  }).join('');
+  }
+      }
 
 
