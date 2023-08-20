5e3292882a38cd1b3996392aec9feebd2bb3e256
Stereotype89 <sh.alexandr.1989@gmail.com>
2023-08-20 22:02:18 +0200

    fix hero2

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,1 +1,25 @@
-
+const div = document.querySelector('.image-content');
+function events() {
+  fetch(`https://tasty-treats-backend.p.goit.global/api/events`)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      murkap(res);
+    });
+}
+events();
+function murkap(mar) {
+  div.insertAdjacentHTML(
+    'beforeend',
+    mar
+      .map(mar => {
+        return `<div>${mar.topic.area}</div>
+              <div>${mar.topic.name}</div>
+              <img  class="name1" src="${mar.topic.previewUrl}" alt="">
+              <img   class="name2" src="${mar.topic.imgWebpUrl}" alt="">
+              <img   class="name3" src="${mar.cook.imgUrl}" alt="">
+              `;
+      })
+      .join('')
+  );
+}
