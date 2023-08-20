aa35659982a5aaa5a697c268040672de5095538c
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2023-08-21 01:04:50 +0300

    update header & create favorites

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,14 @@
+const toggle = document.querySelector('.switch-input[type="checkbox"]');
 
+        function setTheme(themeName) {
+            localStorage.setItem('theme', themeName);
+            document.body.className = themeName;
+        }
+
+        function toggleTheme() {
+            if (localStorage.getItem('theme') === "light") {
+                setTheme("dark");
+            } else {
+                setTheme("light");
+            }
+        }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-
+import './components/Header/Header.js'
+import './components/Header/mobile_menu.js'
 import './components/Categories/Categories.js'
 
