0fa800065e9ebe08542b542d4e150638a76e2246
andriy <evicandrij@gmail.com>
2023-08-21 19:00:11 +0300

    comit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { popularRecipesRendering } from './components/PopularRecipes/PopularRecipes.js';
 import './components/Header/Header.js'
 import './components/Header/header-menu.js'
-import './components/Categories/Categories.js'
+import './components/Categories/categorie2.js'
 import './components/Hero/Hero.js'
 import './components/Hero/swiper.js'
 
