eb76f7f7d38c6f7e51883f2df984c801b7c096ef
andriy <evicandrij@gmail.com>
2023-12-17 09:17:19 +0200

    Revert "comit"
    This reverts commit e7b40d514999f89ef9189928812b3637d8e03868.

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -245,7 +245,7 @@ function showRecipesAdapt() {
     refs.gallery.innerHTML = createGalleryCard(data.results);
   });
 }
-let page = 1;
+
 // pagination /////////////////////////// pagination/////////////////////////// pagination
 refs.button1.addEventListener('click', e => {
   axiosCardInstance.page = 1;
@@ -258,7 +258,6 @@ refs.button2.addEventListener('click', e => {
   if (totalPages === 2) {
     return;
   } else {
-    page = 2
     axiosCardInstance.page = e.currentTarget.innerText;
 
     axiosCardInstance.getCardData().then(data => {
@@ -270,7 +269,6 @@ refs.button3.addEventListener('click', e => {
   if (totalPages === 3) {
     return;
   }
-  page = 3
   axiosCardInstance.page = e.currentTarget.innerText;
 
   axiosCardInstance.getCardData().then(data => {
@@ -282,8 +280,6 @@ refs.btn_right.addEventListener('click', e => {
   if (totalPages === axiosCardInstance.page) {
     return;
   }
-  page++
-  console.log(page)
   axiosCardInstance.page++;
   axiosCardInstance.getCardData().then(data => {
     totalPages = data.totalPages;
@@ -291,8 +287,7 @@ refs.btn_right.addEventListener('click', e => {
   });
 });
 refs.btn_end.addEventListener('click', e => {
-  page = totalPages
-    console.log(page)
+
   axiosCardInstance.page = totalPages;
   axiosCardInstance.getCardData().then(data => {
     refs.gallery.innerHTML = createGalleryCard(data.results);
@@ -302,8 +297,6 @@ refs.btn_left.addEventListener('click', e => {
   if (axiosCardInstance.page === 1) {
     return;
   }
-  page--;
-  console.log(page)
   axiosCardInstance.page--;
   axiosCardInstance.getCardData().then(data => {
     refs.gallery.innerHTML = createGalleryCard(data.results);
@@ -311,8 +304,6 @@ refs.btn_left.addEventListener('click', e => {
 });
 
 refs.btn_start.addEventListener('click', e => {
-  page = 1;
-    console.log(page)
   axiosCardInstance.page = 1;
   axiosCardInstance.getCardData().then(data => {
     refs.gallery.innerHTML = createGalleryCard(data.results);
